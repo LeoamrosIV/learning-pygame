32f973f26c7a104aeef962a785933042186c353d
LeoamrosIV <lamirante@develer.com>
2022-05-15 10:55:09 +0200

    Show background

--- src/core/game.py
+++ src/core/game.py
@@ -7,8 +7,9 @@ import sys
 
 import pygame as pg
 
-from data import SCREEN_RES, GAME_TITLE, MAX_FPS
-from utils import print_event
+from data import (SCREEN_RES, GAME_TITLE, MAX_FPS,
+                  Area, P, PTuple,
+                  get_background_sprite, get_font)
 
 
 class Game:
@@ -16,27 +17,28 @@ class Game:
     Main game class, starts and runs the game.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         # Initialize all imported pygame modules.
         pg.init()
 
         self._init_window()
+        self._init_entities()
         self._run_game_loop()
 
-    def _process_events(self, dt: int) -> None:
+    def _init_window(self) -> None:
         """
-        Processes game events.
-
-        To be called at each iteration of the game loop.
-
-        :param dt: delta time.
+        Initialize window setting screen resolution and window title.
         """
-        for event in pg.event.get():
-            print_event(event)
+        self.screen = pg.display.set_mode(SCREEN_RES)
+        pg.display.set_caption(GAME_TITLE)
 
-            if self._is_quit_button(event):
-                # Exit the game
-                sys.exit()
+    def _init_entities(self) -> None:
+        """
+        Initializes persistent entities.
+        """
+        self.font = get_font(50)
+        self.sky = get_background_sprite("sky.png")
+        self.ground = get_background_sprite("ground.png")
 
     def _run_game_loop(self) -> None:
         """
@@ -47,12 +49,35 @@ class Game:
         while True:
             # Limit FPS and calculate delta time
             dt = clock.tick(MAX_FPS)
+            self._process_events(dt)
+            self._update_screen()
+
+    def _process_events(self, dt: int) -> None:
+        """
+        Processes game events.
+
+        To be called at each iteration of the game loop.
+
+        :param dt: delta time.
+        """
+        for event in pg.event.get():
+
+            if self._is_quit_button(event):
+                # Exit the game
+                sys.exit()
+
+    def _update_screen(self) -> None:
+        self.screen.blit(self.sky, PTuple(0, 0))
+        self.screen.blit(self.ground, PTuple(0, self.sky.get_size()[1]))
+
+        while True:
+            # Limit FPS and calculate delta time
+            dt = clock.tick(MAX_FPS)
 
             self._process_events(dt)
 
-            # Draw all our elements
-            # Update everything
-            pg.display.update()
+        # Draw elements on display
+        pg.display.update()
 
     @staticmethod
     def _is_quit_button(event: pg.event.Event) -> bool:
@@ -63,11 +88,3 @@ class Game:
         :return: `True` if user triggered a quit event.
         """
         return event.type == pg.QUIT or (event.type == pg.KEYUP and event.dict["key"] == pg.K_ESCAPE)
-
-    @staticmethod
-    def _init_window() -> None:
-        """
-        Initialize window setting screen resolution and window title.
-        """
-        pg.display.set_mode(SCREEN_RES)
-        pg.display.set_caption(GAME_TITLE)
