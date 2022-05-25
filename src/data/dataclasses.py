d902645e9295a445d9a64a52290babbf846bc868
LeoamrosIV <lamirante@develer.com>
2022-05-25 22:05:09 +0200

    Remove unused dataclasses

--- src/data/dataclasses.py
+++ src/data/dataclasses.py
@@ -4,7 +4,6 @@
 Contains data classes.
 """
 from typing import NamedTuple
-from dataclasses import dataclass
 
 
 class Area(NamedTuple):
@@ -13,23 +12,3 @@ class Area(NamedTuple):
     """
     width: int
     height: int
-
-
-class PTuple(NamedTuple):
-    """
-    Simple named tuple to represent a point (x, y).
-    """
-    x: int
-    y: int
-
-
-@dataclass(order=True)
-class P:
-    """
-    Simple dataclass to represent a point (x, y).
-    """
-    x: int
-    y: int
-
-    def __call__(self):
-        return PTuple(self.x, self.y)
