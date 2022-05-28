4359cf661d9784f03550cfb01185e46c93fff209
LeoamrosIV <lamirante@develer.com>
2022-05-28 17:52:11 +0200

    Add `GRAVITY` and directories constants

--- src/data/const.py
+++ src/data/const.py
@@ -7,11 +7,18 @@ import os
 
 from . import Area
 
+# Display
 SCREEN_RES = Area(800, 400)
 GAME_TITLE = "Sup?"
 MAX_FPS = 60
 
-ASSETS_DIR = "assets"
+# Directories
+DATA_DIR = os.path.dirname(__file__)
+SRC_DIR = os.path.realpath(os.path.join(DATA_DIR, ".."))
+ASSETS_DIR = os.path.join(SRC_DIR, "assets")
 GRAPHICS_DIR = os.path.join(ASSETS_DIR, "graphics")
 AUDIO_DIR = os.path.join(ASSETS_DIR, "audio")
 FONT_DIR = os.path.join(ASSETS_DIR, "font")
+
+# Gameplay
+GRAVITY = 0.04
